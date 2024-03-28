b5a52e8450b35972005e000a55ef783e6c074e0d
Bogdan Petrov <bogdan_emdetei_petrov@mail.ru>
2024-03-28 16:31:28 +0300

    link

--- src/components/helpers/axios/index.ts
+++ src/components/helpers/axios/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-export const API_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:4444';
+// export const API_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:4444';
+export const API_URL = 'https://todobk-beta.vercel.app';
+
 // export const API_URL = 'http://localhost:4444';
 
 const instance = axios.create(
